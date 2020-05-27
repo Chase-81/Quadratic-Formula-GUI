cbe3c71093bfc85a9d0293789d334f7f4b9c0e00
Chase-81 <winderc@stu.otc.school.nz>
2020-05-28 10:40:46 +1200

    Code layout changed

--- Quadratic formula GUI.py
+++ Quadratic formula GUI.py
@@ -4,8 +4,7 @@ from tkinter import *
 from tkinter import ttk
 
 #calculates discriminant and clears inputs if ticked box is ticked
-def discriminant():
-   global a_value, b_value, c_value
+def discriminant(a_value, b_value, c_value):
    a = a_value.get()
    b = b_value.get()
    c = c_value.get()
@@ -21,8 +20,7 @@ def discriminant():
        x_positive.set("")
 
 #calculates roots of the equation and checks the validity
-def roots():
-   global a_value, b_value, c_value
+def roots(a_value, b_value, c_value):
    a = a_value.get()
    b = b_value.get()
    c = c_value.get()
@@ -41,14 +39,15 @@ def roots():
    else:
        error_message.set("Error: The discriminant is less than zero, there are no real roots for this equation.")
 
+#Creates window and input and output frames
 root = Tk()
 root.title("Quadratic Formula Solver")
 
-statement = ttk.Label(root, text="This calculator can solve quadratic equations in the form ax^2 + bx + c\nEnter the values for a, b, and c")
-statement.grid(row=0, column=0, sticky="NSEW")
-
 input_frame = ttk.LabelFrame(root, text="Inputs")
 input_frame.grid(row=2, column=0, sticky="NSEW")
+
+output_frame = ttk.LabelFrame(root, text="Output")
+output_frame.grid(row=3, column=0, sticky="NSEW")
 
 #variables of program
 a_value = DoubleVar()
@@ -68,9 +67,9 @@ x_negative.set("")
 error_message = StringVar()
 
 
-
-photo_label = Label(root)
-photo_label.grid(row=1, column=0, sticky="NSEW")
+#GUI layout code
+statement = ttk.Label(root, text="This calculator can solve quadratic equations in the form ax^2 + bx + c\nEnter the values for a, b, and c")
+statement.grid(row=0, column=0, sticky="NSEW")
 
 a_label = ttk.Label(input_frame, text="Enter value for a:")
 a_label.grid(row=0, column=0)
@@ -90,14 +89,11 @@ c_label.grid(row=2, column=0)
 c_entry = ttk.Entry(input_frame, textvariable=c_value)
 c_entry.grid(row=2, column=1)
 
-entry_button = Button(input_frame, text="Submit", command=lambda:[discriminant(), roots()])
+entry_button = ttk.Button(input_frame, text="Submit", command=lambda:[discriminant(a_value, b_value, c_value), roots(a_value, b_value, c_value)])
 entry_button.grid(row= 3, column=0)
 
 clear = ttk.Checkbutton(input_frame, text="Clear when submitted", variable=clear_var)
 clear.grid(row=3, column=1)
-
-output_frame = ttk.LabelFrame(root, text="Output")
-output_frame.grid(row=3, column=0, sticky="NSEW")
 
 discriminant_label = ttk.Label(output_frame, text="Discriminant: ")
 discriminant_label.grid(row=0, column=0)
