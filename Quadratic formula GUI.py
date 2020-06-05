8b838b4086ff93533e8a2c00621ad4b8503131ed
Chase-81 <winderc@stu.otc.school.nz>
2020-06-05 15:19:38 +1200

    Initial rounding code

--- Quadratic formula GUI.py
+++ Quadratic formula GUI.py
@@ -5,14 +5,15 @@ from tkinter import ttk
 from tkinter import messagebox
 
 
-#calculates discriminant and clears inputs if ticked box is ticked
+#calculates discriminant and clears inputs if ticked box is ticked and checks the validity
 def discriminant(a_value, b_value, c_value):
    try:
        a = a_value.get()
        b = b_value.get()
        c = c_value.get()
        value = (b ** 2) - (4 * a * c)
-       discriminant_return.set(value)
+       discriminant_return.set()
+
 
        if clear_var.get() == 1:
            a_value.set("")
@@ -21,9 +22,9 @@ def discriminant(a_value, b_value, c_value):
            discriminant_return.set("")
            x_negative.set("")
            x_positive.set("")
+
    except:
        messagebox.showerror("ERROR", "You have to enter 1 real number in each entry field")
-
 
 #calculates roots of the equation and checks the validity
 def roots(a_value, b_value, c_value):
@@ -43,6 +44,8 @@ def roots(a_value, b_value, c_value):
            x_positive.set(result_positive)
            x_negative.set(result_negative)
            error_message.set("")
+       else:
+           error_message.set("Your equation does not have any real roots")
    except:
        pass
 
@@ -60,6 +63,7 @@ def clear_all():
 root = Tk()
 root.title("Quadratic Formula Solver")
 
+
 input_frame = ttk.LabelFrame(root, text="Inputs")
 input_frame.grid(row=2, column=0, sticky="NSEW")
 
@@ -68,6 +72,7 @@ control_frame.grid(row=3, column=0, sticky="NESW")
 
 output_frame = ttk.LabelFrame(root, text="Output")
 output_frame.grid(row=4, column=0, sticky="NSEW")
+
 
 #variables of program
 a_value = DoubleVar()
@@ -85,12 +90,16 @@ x_positive.set("")
 x_negative = DoubleVar()
 x_negative.set("")
 error_message = StringVar()
+round_num = IntVar()
+round_num.set("")
 
 
 #GUI layout code
 statement = ttk.Label(root, text="This calculator can solve quadratic equations in the form ax^2 + bx + c\nEnter the values for a, b, and c")
 statement.grid(row=0, column=0, sticky="NSEW")
 
+
+#Entry labels and entry boxes
 a_label = ttk.Label(input_frame, text="Enter value for a:")
 a_label.grid(row=0, column=0)
 
@@ -109,6 +118,14 @@ c_label.grid(row=2, column=0)
 c_entry = ttk.Entry(input_frame, textvariable=c_value)
 c_entry.grid(row=2, column=1)
 
+round_label = ttk.Label(input_frame, text="Enter how many places to round to:  ")
+round_label.grid(row=3, column=0)
+
+round_entry = ttk.Entry(input_frame, textvariable=round_num)
+round_entry.grid(row=3, column=1)
+
+
+#Buttons and checkbox
 entry_button = ttk.Button(control_frame, text="Submit", command=lambda:[discriminant(a_value, b_value, c_value), roots(a_value, b_value, c_value)])
 entry_button.grid(row= 0, column=0)
 
@@ -118,6 +135,8 @@ clear.grid(row=0, column=1)
 clear_button = ttk.Button(control_frame, text="Clear all", command=clear_all)
 clear_button.grid(row=0, column=2)
 
+
+#Outputs
 discriminant_label = ttk.Label(output_frame, text="Discriminant: ")
 discriminant_label.grid(row=0, column=0)
 
@@ -138,6 +157,7 @@ x_value_negative.grid(row=2, column=1)
 
 error_message_label = ttk.Label(output_frame, textvariable=error_message)
 error_message_label.grid(row=3, column=0)
+
 
 #loops through each item in each frame and applies spacing
 for widget in input_frame.winfo_children():
